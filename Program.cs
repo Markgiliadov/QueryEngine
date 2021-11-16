19ea5caae88470ca64c58d25b63125fc141a7c9f
Mark giliadov <markgiliadov@gmail.com>
2021-11-16 22:19:35 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C__training
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
